d5cfbf0dc88d77a18f51cd28189e75e19d767d73
Feaxmutch <feaxmutch@gmail.com>
2024-10-16 14:34:03 +0300

    Copyed the old solution

--- ReadInt/Program.cs
+++ ReadInt/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            int parsedNumber = ReadInt();
+            Console.WriteLine(parsedNumber);
+        }
+
+        private static int ReadInt()
+        {
+            string userInput = string.Empty;
+            int result;
+
+            while (int.TryParse(userInput, out result) == false)
+            {
+                Console.Clear();
+                Console.Write("Введите число: ");
+                userInput = Console.ReadLine();
+            }
+
+            return result;
         }
     }
 }
